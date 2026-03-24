ba58d1ad1d2510cd57b81e2cafb423ba021433d2
Beatriz <biaperigo01@gmail.com>
2026-03-24 11:44:27 -0300

    feat: declarações e variaveis JS

---
+++ src/js/script.js
@@ -0,0 +1,50 @@
+// Declarações e variaveis
+
+//var, let e const
+
+// var x = 10;
+// console.log(x);
+// var x = 20;
+// console.log(x)
+// var pode redeclarar e acessar fora do escopo
+// let nao pode redeclarar e nao consegue acessar fora do escopo
+
+// if (true){
+//     var nome = "fiap";
+// }
+// console.log(nome)
+
+//const - nao deixa alterar dado nenhum
+// const taxaJuros = 0.85;
+// taxaJuros = 0.50;
+// console.log(taxaJuros)
+
+
+var exemplo1 = "Ola dev var";
+console.log(exemplo1);
+
+let exemplo2 = "ola dev let";
+console.log(exemplo2);
+
+const exemplo3 = "ola deve const";
+console.log(typeof exemplo3);
+
+let exemplo4 = 20;
+console.log(typeof exemplo4);
+
+let exemplo5 = true;
+console.log(typeof exemplo5);
+
+let exemplo6 = {npme:"fiap"};
+console.log(exemplo6);
+
+let exemplo7 = ["java", "python", "C#"];
+console.log(exemplo7);
+
+//variavel indefinida
+let exemplo8;
+console.log(exemplo8);
+
+//variavel vazia 
+let exemplo9 = null;
+console.log(exemplo9)
