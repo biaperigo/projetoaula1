dbc6ff3a6e3f8cb4301ea0d09307136d8c931585
Beatriz <biaperigo01@gmail.com>
2026-03-31 10:37:15 -0300

    feat: operadores aritmeticos, logicos e comparacao

--- src/js/script.js
+++ src/js/script.js
@@ -20,31 +20,57 @@
 // console.log(taxaJuros)
 
 
-var exemplo1 = "Ola dev var";
-console.log(exemplo1);
+//var exemplo1 = "Ola dev var";
+//console.log(exemplo1);
 
-let exemplo2 = "ola dev let";
-console.log(exemplo2);
+//let exemplo2 = "ola dev let";
+//console.log(exemplo2);
 
-const exemplo3 = "ola deve const";
-console.log(typeof exemplo3);
+//const exemplo3 = "ola deve const";
+//console.log(typeof exemplo3);
 
-let exemplo4 = 20;
-console.log(typeof exemplo4);
+///let exemplo4 = 20;
+//console.log(typeof exemplo4);
 
-let exemplo5 = true;
-console.log(typeof exemplo5);
+//let exemplo5 = true;
+//console.log(typeof exemplo5);
 
-let exemplo6 = {npme:"fiap"};
-console.log(exemplo6);
+//let exemplo6 = {npme:"fiap"};
+//console.log(exemplo6);
 
-let exemplo7 = ["java", "python", "C#"];
-console.log(exemplo7);
+//let exemplo7 = ["java", "python", "C#"];
+//console.log(exemplo7);
 
 //variavel indefinida
-let exemplo8;
-console.log(exemplo8);
+//let exemplo8;
+//console.log(exemplo8);
 
 //variavel vazia 
-let exemplo9 = null;
-console.log(exemplo9)
+//let exemplo9 = null;
+//console.log(exemplo9)
+
+// Operadores aritmeticos
+
+const valor1 = 20;
+const valor2 = 10;
+
+console.log(valor1 + valor2);
+console.log(valor1 / valor2);
+console.log(valor1 - valor2);
+console.log(valor1 * valor2);
+
+//operadores logicos
+
+console.log(valor1 > valor2);
+console.log(valor1>10 && valor2 != 20);
+// && - E (os dois verdadeiros)   || - OU (um deles verdadeiro)
+console.log(valor2 > 5 || valor1 <50);
+
+//operadores de comparação
+console.log(valor1 < valor2);
+console.log(valor1 >= valor2);
+console.log(valor1 <= valor2);
+console.log(valor1 != valor2);
+console.log(valor1 == valor2);
+console.log(valor1 === valor2);
+console.log("O valor é:",valor1)
